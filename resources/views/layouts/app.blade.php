24ef168502c4d6aa1b9383ef1170454e1dd1c050
5hau5 <chili.shaus@gmail.com>
2025-07-14 05:53:08 +0500

    alot of stuff that i forgo to commit for a while on

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,21 +1,36 @@
 <!DOCTYPE html>
-<html>
-<head>
-    <title>Picnic Island</title>
-</head>
-<body>
-    <nav>
-        <a href="{{ route('visitor.home') }}">Home</a>
-        <a href="{{ route('visitor.hotel.book') }}">Hotel</a>
-        <a href="{{ route('visitor.ferry.book') }}">Ferry</a>
-        <a href="{{ route('visitor.park.tickets') }}">Park</a>
-        <a href="{{ route('visitor.map') }}">Map</a>
-    </nav>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <hr>
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <div>
-        @yield('content')
-    </div>
-</body>
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="font-sans antialiased">
+        <div class="min-h-screen bg-gray-100">
+            @include('layouts.navigation')
+
+            <!-- Page Heading -->
+            @isset($header)
+                <header class="bg-white shadow">
+                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                        {{ $header }}
+                    </div>
+                </header>
+            @endisset
+
+            <!-- Page Content -->
+            <main>
+                {{ $slot }}
+            </main>
+        </div>
+    </body>
 </html>
---
+++ resources/views/visitor/hotels/book.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>book for hotels</title>
+</head>
+<body>
+    <h1>bal</h1>
+</body>
+</html>
